76bb4c5f95c02ec7d4d1933d0f8f687055325dc1
Ivan G <ivan.s.grishulenko@gmail.com>
2018-09-08 19:18:56 +0700

    Add validation on max count of players in game

--- FeatureBan.Domain.Tests/GameTests.cs
+++ FeatureBan.Domain.Tests/GameTests.cs
@@ -18,13 +18,28 @@ namespace FeatureBan.Domain.Tests
         }
 
         [Fact]
-        public void AddPlayer_ThrowsInvalidOperationException_WhenPlayerIsAlredyInGame()
+        public void AddPlayer_ThrowsInvalidOperationException_WhenPlayerIsAlreadyInGame()
         {
             var game = new Game();
             var player = new Player("foo");
             game.AddPlayer(player);
 
             Assert.Throws<InvalidOperationException>(() => game.AddPlayer(player));
+        }
+
+        [Fact]
+        public void AddPlayer_ThrowsInvalidOperationException_WhenMaxPlayerCountExceeded()
+        {
+            var game = new Game(maxPlayerCount: 3);
+            var player1 = new Player("a");
+            var player2 = new Player("b");
+            var player3 = new Player("c");
+            var player4 = new Player("d");
+            game.AddPlayer(player1);
+            game.AddPlayer(player2);
+            game.AddPlayer(player3);
+
+            Assert.Throws<InvalidOperationException>(() => game.AddPlayer(player4));
         }
     }
 }
--- FeatureBan.Domain/Game.cs
+++ FeatureBan.Domain/Game.cs
@@ -7,12 +7,20 @@ namespace FeatureBan.Domain
     public class Game
     {
         private readonly IList<Player> _players = new List<Player>();
+        private readonly int _maxPlayerCount;
+
+        public Game(int maxPlayerCount = 5)
+        {
+            this._maxPlayerCount = maxPlayerCount;
+        }
 
         public int CountOfPlayers => _players.Count;
 
         public void AddPlayer(Player player)
         {
             if (_players.Any(x => x.Id == player.Id))
+                throw new InvalidOperationException();
+            if (_players.Count + 1 > _maxPlayerCount)
                 throw new InvalidOperationException();
 
             _players.Add(player);
